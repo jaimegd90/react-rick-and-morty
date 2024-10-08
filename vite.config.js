3c79e262d4637f9bc416ccfb944c3d00f8888aa6
JaimeGon <jaimegd90@gmail.com>
2024-10-08 11:17:14 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/jaimegd90/vite-react-rick-and-morty",
+  base: "https://jaimegd90.github.io/vite-react-rick-and-morty/",
   plugins: [react()],
 })
